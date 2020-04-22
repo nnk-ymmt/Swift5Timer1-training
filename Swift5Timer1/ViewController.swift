c8dec255f01280c00356a1cf71d184cf3267c59b
Nonoka Yamamoto <garden.n1234567890@gmail.com>
2020-04-22 13:01:56 +0900

    Initial Commit

---
+++ Swift5Timer1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5Timer1
+//
+//  Created by 山本ののか on 2020/04/22.
+//  Copyright © 2020 Nonoka Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
